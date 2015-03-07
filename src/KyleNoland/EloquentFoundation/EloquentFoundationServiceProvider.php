5d9a71cb05133cbd8da95a312cc4b3be2c9cb8b7
Kyle Noland <kylenoland@gmail.com>
2015-03-06 21:54:35 -0600

    Initial commit

---
+++ src/KyleNoland/EloquentFoundation/EloquentFoundationServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace KyleNoland\EloquentFoundation;
+
+use Illuminate\Support\ServiceProvider;
+
+class EloquentFoundationServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('kyle-noland/eloquent-foundation');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
