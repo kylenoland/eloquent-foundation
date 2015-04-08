5f61b68789d3ca5ae348aa6ca4db93dac15a0b22
Kyle Noland <kylenoland@gmail.com>
2015-04-07 23:04:43 -0500

    Added SnailMailableTrait

---
+++ src/KyleNoland/EloquentFoundation/Traits/SnailMailableTrait.php
@@ -0,0 +1,48 @@
+<?php namespace KyleNoland\EloquentFoundation\Traits;
+
+trait SnailMailableTrait {
+
+	/**
+	 * @return string
+	 */
+	public function getStreetAddress()
+	{
+		$address = $this->address1;
+
+		if( ! is_null($this->address2))
+		{
+			$address .= ', '. $this->address2;
+		}
+
+		return $address;
+	}
+
+
+	/**
+	 * @return string
+	 */
+	public function getVicinity()
+	{
+		return $this->city . ', ' . $this->state . ' ' . $this->zip;
+	}
+
+
+	/**
+	 * @param bool $implode
+	 *
+	 * @return array|string
+	 */
+	public function getAddress($implode = true)
+	{
+		$address[] = $this->getStreetAddress();
+		$address[] = $this->getVicinity();
+
+		if($implode)
+		{
+			return implode('<br>', $address);
+		}
+
+		return $address;
+	}
+
+}
